7fbbf515a19faf50173824f76950c44b4bf7c043
Haidahazmi <haidahazmi@gmail.com>
2025-05-27 01:23:25 +0800

    Add files via upload

---
+++ MODULE 3/deleteSlot.php
@@ -0,0 +1,27 @@
+<?php
+// Connect to the database server
+$link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
+
+// Select the database
+mysqli_select_db($link, "mypetakom") or die(mysqli_error($link)); // Change db name accordingly
+
+// Get the ID from the URL
+if (!isset($_GET['id'])) {
+    die("Missing attendance slot ID.");
+}
+
+$idURL = $_GET['id'];
+
+// SQL query to delete the record
+$query = "DELETE FROM attendance_slot WHERE attendanceslot_id = '$idURL'";
+
+// Execute the delete query
+$result = mysqli_query($link, $query);
+
+// Redirect or show alert if successful
+if ($result) {
+    echo "<script type='text/javascript'> alert('Record deleted successfully.'); window.location='attendaceSlot.php'; </script>";
+} else {
+    echo "Error deleting record: " . mysqli_error($link);
+}
+?>
